888fb3948a205fc5a72ea3b4a3352024021b7d90
Alexis Loiselle <alexis@t-b.ca>
2023-12-18 13:16:30 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
+// Ideas
+
+// Scoring heuristic based on the game description
+
+// Minimax as it's a zero sum game
+// Alpha beta pruning to reduce the number of nodes to explore
+// To use when the strategy should be deterministic
+
+// MCTS could be useful because of the number of possible states
+// Relevant because of the randomness in which fishes move
+// To use when the strategy should be stochastic
+
+/**
+ * Score points by scanning valuable fish faster than your opponent.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let creature_count = parse_input!(input_line, i32);
+    for i in 0..creature_count as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let creature_id = parse_input!(inputs[0], i32);
+        let color = parse_input!(inputs[1], i32);
+        let _type = parse_input!(inputs[2], i32);
+    }
+
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let my_score = parse_input!(input_line, i32);
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let foe_score = parse_input!(input_line, i32);
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let my_scan_count = parse_input!(input_line, i32);
+        for i in 0..my_scan_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let creature_id = parse_input!(input_line, i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let foe_scan_count = parse_input!(input_line, i32);
+        for i in 0..foe_scan_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let creature_id = parse_input!(input_line, i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let my_drone_count = parse_input!(input_line, i32);
+        for i in 0..my_drone_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let drone_id = parse_input!(inputs[0], i32);
+            let drone_x = parse_input!(inputs[1], i32);
+            let drone_y = parse_input!(inputs[2], i32);
+            let emergency = parse_input!(inputs[3], i32);
+            let battery = parse_input!(inputs[4], i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let foe_drone_count = parse_input!(input_line, i32);
+        for i in 0..foe_drone_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let drone_id = parse_input!(inputs[0], i32);
+            let drone_x = parse_input!(inputs[1], i32);
+            let drone_y = parse_input!(inputs[2], i32);
+            let emergency = parse_input!(inputs[3], i32);
+            let battery = parse_input!(inputs[4], i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let drone_scan_count = parse_input!(input_line, i32);
+        for i in 0..drone_scan_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let drone_id = parse_input!(inputs[0], i32);
+            let creature_id = parse_input!(inputs[1], i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let visible_creature_count = parse_input!(input_line, i32);
+        for i in 0..visible_creature_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let creature_id = parse_input!(inputs[0], i32);
+            let creature_x = parse_input!(inputs[1], i32);
+            let creature_y = parse_input!(inputs[2], i32);
+            let creature_vx = parse_input!(inputs[3], i32);
+            let creature_vy = parse_input!(inputs[4], i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let radar_blip_count = parse_input!(input_line, i32);
+        for i in 0..radar_blip_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let drone_id = parse_input!(inputs[0], i32);
+            let creature_id = parse_input!(inputs[1], i32);
+            let radar = inputs[2].trim().to_string();
+        }
+        for i in 0..my_drone_count as usize {
+            // Write an action using println!("message...");
+            // To debug: eprintln!("Debug message...");
+
+            println!("WAIT 1"); // MOVE <x> <y> <light (1|0)> | WAIT <light (1|0)>
+        }
+    }
+}
